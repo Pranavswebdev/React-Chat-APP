a22edc285b4b541a47046c720dd1a7f573540ece
pranavz <mailz4pranav@gmail.com>
2022-03-02 20:07:20 +0530

    chat started

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Stack from '@mui/material/Stack';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+     
+<Button variant='contained'  style={{background:'#ab1941'}} > Hello MUI </Button>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+ const express = require('express');
+ const app = express();
+const {chats} = require ('./data/data')
+
+app.get('/api/chat',(req,res)=>{
+    
+res.send(chats)
+
+})
+
+
+app.get('/api/chat/:id',(req,res)=>{
+
+    console.log(req.params.id);
+
+   const singlechat =  chats.find((c)=>c._id === req.params.id);
+   res.send(singlechat)
+
+})
+
+ app.listen(5000,console.log('running in port 5000'));
